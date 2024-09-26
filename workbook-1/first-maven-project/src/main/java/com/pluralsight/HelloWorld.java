f8676f0af71043d6a72f54da12dc03c3a933922a
Jovaughn Douglas <jovaughn.douglas@gmail.com>
2024-09-26 10:23:36 -0400

    Shopping List done

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //print hello world to the terminal
+     System.out.println("Hello World!");
+    }
+
+
+}
---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Jovaughn's List");
+        System.out.println(" Eggs, Detergent, Pizza, Pasta, Paper, Snickers, Pops, Water, Milk, Bread");
+
+    }
+}
